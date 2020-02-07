0d0129d5100ca9e206dc8098ce8e5a3c1fddd4e3
DaedalusCommunity <53686789+DaedalusCommunity@users.noreply.github.com>
2020-02-07 15:59:19 +0100

    Add files via upload

---
+++ CratersEditor.cs
@@ -0,0 +1,31 @@
+﻿#if UNITY_EDITOR
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(Craters))]
+public class CratersEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+
+        Craters myScript = (Craters)target;
+        if (GUILayout.Button("Crater"))
+        {
+            myScript.GenCrater();
+        }
+
+        if (GUILayout.Button("Smooth"))
+        {
+            myScript.Smooth();
+        }
+        if (GUILayout.Button("Rain"))
+        {
+            myScript.MakeItRain();
+        }
+
+    }
+}
+#endif
